fb749f6ce6c95daed3dfa3979884ad717783b83c
Sami <samialeksi.tuomela@gmail.com>
2022-06-28 20:12:46 +0300

    added 6.21

--- osa6/redux-anecdotes/src/reducers/notificationReducer.js
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,13 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = ''
+let lastTimeout = null
 
 const noteSlice = createSlice({
     name: 'notification',
     initialState,
     reducers: {
         setInternalNotification(state, action) {
-            
+            clearTimeout(lastTimeout)
             return action.payload
         },
         removeNotification(state, action) {
@@ -19,7 +20,7 @@ const noteSlice = createSlice({
 export const setNotification = (action, time) => {
     return dispatch => {
         dispatch(setInternalNotification(action))
-        setTimeout(() => dispatch(removeNotification()), time*1000)
+        lastTimeout = setTimeout(() => dispatch(removeNotification()), time*1000)
     }
 }
 export const {  setInternalNotification, removeNotification } = noteSlice.actions
